e1e1212ae6ec1c573e5ea5ab2e42be5608f6f84a
Amrutha <24wh1a05at@bvrithyderabad.edu.in>
2026-07-03 13:41:42 +0530

    update port

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const express = require('express')
 
 const app = express()
 
-const PORT = 3002;
-
+const PORT = process.env.PORT || 3002
+app.use(express.static("frontend"))
 app.listen(PORT, function(){
   console.log("succesfully runing at http://localhost:" +PORT)
 })
-app.use(express.static("frontend"))
-app.listen
+
